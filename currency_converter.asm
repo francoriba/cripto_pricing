54ff27ecff65896c7126d2a600f23edab7587365
francoriba <franco.riba@mi.unc.edu.ar>
2023-04-11 22:25:43 -0300

    Funciona comiplando para x86 pero hay que ver el tema de compatibilidad con interprete Python de 64

--- currency_converter.asm
+++ currency_converter.asm
@@ -1,9 +1,20 @@
 segment .data
+; no usamos segmento de datos
 
 segment .bss
+; no usamos variables sin inicializar
 
 segment .text
     global  convert_to
 
-convert_to: 
+;declaramos macros para la dirección de los bytes mas altos de los dos parametros
+%define p1 [ebp+12] 
+%define p2 [ebp+8]
 
+convert_to: 
+    enter 0, 0 ;no se reserva espacio para variables locales
+    fld     dword p1 ;ST0 = p1
+    fld     dword p2 ;ST1 = p2
+    fmulp   st1, st0 ;multiplicamos la cotización de la mondea fiduciaria por el valor de la cripto
+    leave ;liberamos la pila
+    ret ;retornamos a la función de c
--- main.c
+++ main.c
@@ -1,21 +1,50 @@
-#include "cdecl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int PRE_CDECL asm_main( void ) POST_CDECL; 
-double PRE_CDECL convert_to( double, double) POST_CDECL; 
+// Declaramos la función convert_to, que se definirá en assembler
+extern float convert_to(float , float);
 
-int data_interface(double btc_usd, double eth_usd, double ltc_usd, double rate_ars, double rate_eur){
-    double btc_ars, btc_eur, eth_ars, eth_eur, ltc_ars, ltc_eur; 
+//Prototipos de funciones
+float btc_ars(float btc_usd, float rate_ars);
+float btc_eur(float btc_usd,float rate_eur);
+float eth_ars(float eth_usd, float rate_ars);
+float eth_eur(float eth_usd, float rate_eur);
+float ltc_ars(float ltc_usd, float rate_ars);
+float ltc_eur(float ltc_usd, float rate_eur);
 
-    //bitcoin convertions
-    btc_ars = convert_to(btc_usd, rate_ars);
-    btc_eur = convert_to(btc_usd, rate_eur);
+int main(int argc, char *argv[]){
 
-    //ethereum convertions
-    eth_ars = convert_to(eth_usd, rate_ars);
-    eth_eur = convert_to(eth_usd, rate_eur);
+    printf("%f",btc_ars(30364.108274487993, 387.075));
+    return 0;
+}
 
-    //litecoin convertions
-    ltc_ars = convert_to(ltc_usd, rate_ars);
-    ltc_eur = convert_to(ltc_usd, rate_eur);
-    
-    return btc_ars ;}
+float btc_ars(float btc_usd, float rate_ars){
+    double btc_ars = convert_to(btc_usd, rate_ars); // calcula el precio de btc en ars
+    return btc_ars;
+}
+
+float btc_eur(float btc_usd,float rate_eur){
+    float btc_eur = convert_to(btc_usd, rate_eur); //calcula el precio de btc en eur
+    return btc_eur;
+}
+
+float ltc_ars(float ltc_usd, float rate_ars){
+    float ltc_ars = convert_to(ltc_usd, rate_ars); //calcula el precio de ltc en ars
+    return ltc_ars;
+}
+
+float ltc_eur(float ltc_usd, float rate_eur){
+    float ltc_eur = convert_to(ltc_usd, rate_eur); //calcula el precio de ltc en eur
+    return ltc_eur;
+}
+
+float eth_ars(float eth_usd, float rate_ars){
+    float eth_ars = convert_to(eth_usd, rate_ars); //calcula el precio de eth en ars
+    return eth_ars;
+}
+
+float eth_eur(float eth_usd, float rate_eur){
+    float eth_eur = convert_to(eth_usd, rate_eur); //calcula el precio de eth en eur
+    return eth_eur;
+} 
